17c5b00d355a392d0edd1fb4fb33b694905cfb9a
JTMabutas17 <justin.troy.mabutas@gmail.com>
2021-03-11 22:34:48 -0800

    Updates

---
+++ precache-manifest.a6fc649556e77ee5ed2f1abf47d08067.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9cef065c34b2ba0cb8de",
+    "url": "/Naruto_Portfolio/static/css/main.1eda8ee8.chunk.css"
+  },
+  {
+    "revision": "9cef065c34b2ba0cb8de",
+    "url": "/Naruto_Portfolio/static/js/main.0587e431.chunk.js"
+  },
+  {
+    "revision": "4e0cbf34ea44cd7cbf21",
+    "url": "/Naruto_Portfolio/static/js/runtime~main.fe804c28.js"
+  },
+  {
+    "revision": "bc9e6c5c2187edfaf1a6",
+    "url": "/Naruto_Portfolio/static/css/2.a6ad4708.chunk.css"
+  },
+  {
+    "revision": "bc9e6c5c2187edfaf1a6",
+    "url": "/Naruto_Portfolio/static/js/2.394bc2e4.chunk.js"
+  },
+  {
+    "revision": "cc2fb7839900597146c9bf1d5a849dcf",
+    "url": "/Naruto_Portfolio/index.html"
+  }
+];
